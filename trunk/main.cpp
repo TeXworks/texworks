159c349b79285e798c578ceae415f01439387fbc
jonathan <jonathan@9a2a4a0b-1251-0410-9248-99fc04669920>
2008-02-05 12:43:55 +0000

    win32 build (temp, with hard-coded paths); support filenames on command line

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -5,8 +5,14 @@ int main(int argc, char *argv[])
 {
 	QTeXApp app(argc, argv);
 
-	TeXDocument *mainWin = new TeXDocument;
-	mainWin->show();
+	// first argument is the executable name, so we skip that
+	for (int i = 1; i < argc; ++i)
+		app.open(argv[i]);
+	
+	if (TeXDocument::documentList().size() == 0) {
+		TeXDocument *mainWin = new TeXDocument;
+		mainWin->show();
+	}
 
 	return app.exec();
 }
