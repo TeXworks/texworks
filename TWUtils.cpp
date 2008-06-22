34a556cfab14a4ddd39039f1285ce79b8a08e9a2
jonathan <jonathan@9a2a4a0b-1251-0410-9248-99fc04669920>
2008-06-22 20:58:08 +0000

    updated comments etc

--- TWUtils.cpp
+++ TWUtils.cpp
@@ -12,9 +12,11 @@
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License along
-	with this program; if not, write to the Free Software Foundation, Inc.,
-	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	For links to further information, or to contact the author,
+	see <http://tug.org/texworks/>.
 */
 
 #include "TWUtils.h"
@@ -72,7 +74,7 @@ const QString TWUtils::getLibraryPath(const QString& subdir)
 			QString cwd = QDir::currentPath();
 			if (QDir::setCurrent(libPath)) {
 				// copy default contents from app resources into the library dir
-				QDir resDir(":/resources/" + subdir);
+				QDir resDir(":/resfiles/" + subdir);
 				copyResources(resDir, libPath);
 			}
 			QDir::setCurrent(cwd);
