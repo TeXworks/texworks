c4da03c91b0946634504d91b0eb44e5bf5e90187
jonathan@jfkew.plus.com <jonathan@jfkew.plus.com@9a2a4a0b-1251-0410-9248-99fc04669920>
2008-09-29 20:56:29 +0000

    support for dropping files onto text and pdf windows to open them (thanks to st.loeffler for the initial patch this was based on)

--- trunk/src/PDFDocument.cpp
+++ trunk/src/PDFDocument.cpp
@@ -1458,3 +1458,30 @@ void PDFDocument::goToDestination(const QString& destName)
 	if (pdfWidget)
 		pdfWidget->goToDestination(destName);
 }
+
+void PDFDocument::dragEnterEvent(QDragEnterEvent *event)
+{
+	// Only accept files for now
+	event->ignore();
+	if (event->mimeData()->hasUrls()) {
+		const QList<QUrl> urls = event->mimeData()->urls();
+		foreach (const QUrl& url, urls) {
+			if (url.scheme() == "file") {
+				event->acceptProposedAction();
+				break;
+			}
+		}
+	}
+}
+
+void PDFDocument::dropEvent(QDropEvent *event)
+{
+	event->ignore();
+	if (event->mimeData()->hasUrls()) {
+		const QList<QUrl> urls = event->mimeData()->urls();
+		foreach (const QUrl& url, urls)
+			if (url.scheme() == "file")
+				TWApp::instance()->open(url.path());
+		event->acceptProposedAction();
+	}
+}
