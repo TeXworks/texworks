fe3ada76aa7bfbda0d0396e34b4fd89cb1493568
jonathan@jfkew.plus.com <jonathan@jfkew.plus.com@9a2a4a0b-1251-0410-9248-99fc04669920>
2010-04-03 14:13:58 +0000

    add LaTeXmk to default engine list

--- trunk/src/TWApp.cpp
+++ trunk/src/TWApp.cpp
@@ -585,6 +585,8 @@ void TWApp::setDefaultEngineList()
 	else
 		engineList->clear();
 	*engineList
+		<< Engine("LaTeXmk", "latexmk" EXE, QStringList("-e") << 
+				  "$pdflatex=q/pdflatex -synctex=1 %O %S/" << "-pdf" << "$fullname", true)
 		<< Engine("pdfTeX", "pdftex" EXE, QStringList("$synctexoption") << "$fullname", true)
 		<< Engine("pdfLaTeX", "pdflatex" EXE, QStringList("$synctexoption") << "$fullname", true)
 		<< Engine("XeTeX", "xetex" EXE, QStringList("$synctexoption") << "$fullname", true)
