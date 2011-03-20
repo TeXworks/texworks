e7aac9741b8fb720d68323bbca84d660d78d8155
st.loeffler@gmail.com <st.loeffler@gmail.com@9a2a4a0b-1251-0410-9248-99fc04669920>
2011-03-20 14:14:17 +0000

    Branch 0.4 stable release and bump version numbers

--- src/SvnRev.h
+++ src/SvnRev.h
@@ -1,2 +1,2 @@
-#define SVN_REVISION 755
-#define SVN_REVISION_STR "755"
+#define SVN_REVISION 756
+#define SVN_REVISION_STR "756"
--- src/TWVersion.h
+++ src/TWVersion.h
@@ -20,6 +20,6 @@
 */
 
 #define VER_MAJOR			0
-#define VER_MINOR			3
+#define VER_MINOR			5
 #define VER_BUGFIX			0
-#define TEXWORKS_VERSION	"0.3"
+#define TEXWORKS_VERSION	"0.5"
