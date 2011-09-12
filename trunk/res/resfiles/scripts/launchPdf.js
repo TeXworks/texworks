4654f5d7e0b81c6cf37fb1a646fd38b62fb546a2
st.loeffler@gmail.com <st.loeffler@gmail.com@9a2a4a0b-1251-0410-9248-99fc04669920>
2011-09-12 16:49:41 +0000

    Improve launchPdf script (issue 399)
    In particular when the execution of system commands was disabled the script failed with an empty message box.

--- trunk/res/resfiles/scripts/launchPdf.js
+++ trunk/res/resfiles/scripts/launchPdf.js
@@ -1,10 +1,18 @@
 // TeXworksScript
 // Title: Open in default viewer
 // Description: Attempts to open the PDF file using the system's default viewer
-// Author: Jonathan Kew
-// Version: 0.2
-// Date: 2010-01-26
+// Author: Stefan Löffler & Jonathan Kew
+// Version: 0.2.1
+// Date: 2011-09-12
 // Script-Type: standalone
 // Context: PDFDocument
 
-TW.launchFile(TW.target.fileName);
+
+retVal = TW.launchFile(TW.target.fileName);
+
+if (retVal["status"] != 0)
+{
+	TW.information(TW.target, "Error", retVal["message"]);
+}
+
+undefined;
