80ed0c863aaf5d40c58bb7b2319aedf575b5e90d
st.loeffler@gmail.com <st.loeffler@gmail.com@9a2a4a0b-1251-0410-9248-99fc04669920>
2012-08-22 06:08:25 +0000

    Merge log parser improvements by Antonio (issue 596)
    Changes:
     - Improve handling of badboxes
     - Check what is allowed after a filename.
     - Better handling of spaces and message breaks in warnings.
     - New "debug" category (for \show and \showthe output)

--- trunk/testcases/logParser/tests-miktex/files.js
+++ trunk/testcases/logParser/tests-miktex/files.js
@@ -88,5 +88,6 @@
   "C:\\Users\\Antonio\\Dati\\Università\\Magistrale\\Sicurezza\\Progetto\\project-erinyes\\doc\\relazione.tex",
   "C:\\Users\\Antonio\\Dati\\Università\\Magistrale\\Sicurezza\\Progetto\\project-erinyes\\doc\\relazione.toc",
   "C:/Users/Mark/Documents/untitled-3.tex",
-  "C:\\Users\\Mark\\Documents\\untitled-3.aux"
+  "C:\\Users\\Mark\\Documents\\untitled-3.aux",
+  "F:\\Antonio\\Università\\Magistrale\\Sicurezza\\appunti-sicurezza\\anthosthesis.cls",
 ]
