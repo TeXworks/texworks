c9f0311f29393e371d225f5cc76dc3c8bfc9fc20
st.loeffler@gmail.com <st.loeffler@gmail.com@9a2a4a0b-1251-0410-9248-99fc04669920>
2013-03-01 20:54:38 +0000

    <QtPDF> Update icons
     - add tentative logo (to identify the app in the task switcher)
     - vectorize the hand icon
     - derive fit*.png icons from Tango icons for window and page
     - add page mode icons (derived from Tango page icon)
     - redo zoom-select based on other zoom icons
     - add .svg sources to icons/scalable for future improvements

--- trunk/modules/QtPDF/main.cpp
+++ trunk/modules/QtPDF/main.cpp
@@ -5,6 +5,7 @@
 
 int main(int argc, char **argv) {
   QApplication app(argc, argv);
+  app.setWindowIcon(QIcon(QString::fromUtf8(":/icons/logo.png")));
   PDFViewer mainWin(QString::fromUtf8("pgfmanual.pdf"));
 
   mainWin.show();
