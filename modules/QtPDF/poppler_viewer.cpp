5ec7a97616ddc1bf8f62c97072b6491daba0cdca
st.loeffler@gmail.com <st.loeffler@gmail.com@9a2a4a0b-1251-0410-9248-99fc04669920>
2013-03-01 20:25:29 +0000

    <QtPDF> New class for page images with delayed rendering
    Initialially, each page is loaded with a blank pixmap of the appropriate size.
    Rendering the PDF content to the page is saved for the actual moment that the
    `QGraphicsScene` needs to paint the image.
    
    This is accomplished through `PDFPageGraphicsItem`, a subclass of
    `QGraphicsPixmapItem` that has an overloaded `paint` method.

--- modules/QtPDF/poppler_viewer.cpp
+++ modules/QtPDF/poppler_viewer.cpp
@@ -3,63 +3,93 @@
 
 #include <iostream>
 
+class PDFPageGraphicsItem : public QGraphicsPixmapItem {
+  typedef QGraphicsPixmapItem super;
+  // To spare the need for a destructor
+  const std::auto_ptr<Poppler::Page> page;
+  bool dirty;
+  double dpiX;
+  double dpiY;
+
+public:
+
+  PDFPageGraphicsItem(Poppler::Page *a_page, QGraphicsItem *parent = 0) : super(parent), page(a_page), dirty(true) {
+    dpiX = QApplication::desktop()->physicalDpiX();
+    dpiY = QApplication::desktop()->physicalDpiY();
+
+    // Create an empty pixmap that is the same size as the PDF page. This
+    // allows us to dielay the rendering of pages until they actually come into
+    // view which saves time.
+    QSizeF pageSize = page->pageSizeF() / 72.0;
+    pageSize.setHeight(pageSize.height() * dpiY);
+    pageSize.setWidth(pageSize.width() * dpiX);
+
+    setPixmap(QPixmap(pageSize.toSize()));
+  }
+
+  void paint( QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
+    // If this `PDFPageGraphicsItem` is still using the empty `QPixmap` it was
+    // constructed with, `dirty` will be `true`. We replace the empty pixmap
+    // with a rendered image of the page.
+    if ( this->dirty ) {
+      // `convertFromImage` was previously part of the Qt3 API and was
+      // forward-ported to Qt 4 in version 4.7. Supposidly more efficient than
+      // using `QPixmap::fromImage` as it does not involve constructing a new
+      // QPixmap.
+      //
+      // If the performance hit is not outrageous, may want to consider
+      // downgrading to `QPixmap::fromImage` in order to support more versions
+      // of Qt 4.x.
+      this->pixmap().convertFromImage(this->page->renderToImage(dpiX, dpiY));
+      this->dirty = false;
+    }
+
+    // After checking if page image needs to be rendered, punt call back to the
+    // `paint` method of `QGraphicsPixmapItem`.
+    super::paint(painter, option, widget);
+  }
+
+private:
+  // Parent has no copy constructor, so this class shouldn't either. Also, we
+  // hold some information in an `auto_ptr` which does interesting things on
+  // copy that C++ newbies may not expect.
+  Q_DISABLE_COPY(PDFPageGraphicsItem)
+
+};
+
+
 int main(int argc, char **argv) {
   QApplication app(argc, argv);
 
-  const double dpiX = app.desktop()->physicalDpiX();
-  const double dpiY = app.desktop()->physicalDpiY();
-
-  QMainWindow *mainWin = new QMainWindow();
-  QGraphicsView *viewport = new QGraphicsView(mainWin);
+  QMainWindow mainWin;
+  QGraphicsView *viewport = new QGraphicsView(&mainWin);
   QGraphicsScene *canvas = new QGraphicsScene(viewport);
 
-  viewport->setBackgroundBrush(QBrush(QColor("grey")));
+  viewport->setBackgroundRole(QPalette::Dark);
+  viewport->setAlignment(Qt::AlignCenter);
 
   Poppler::Document *doc = Poppler::Document::load(QString("pgfmanual.pdf"));
   std::cerr << "number of pages: " << doc->numPages() << std::endl;
 
   int i;
   float offY = 0.0;
-  QImage pageImage;
-  QGraphicsItem *pagePtr;
+  PDFPageGraphicsItem *pagePtr;
 
-  pageImage = doc->page(0)->renderToImage(dpiX, dpiY);
-  pagePtr = canvas->addPixmap(QPixmap::fromImage(pageImage));
-
-  pagePtr->setPos(0.0, offY);
-
-  QSizeF pageSize, newSize;
-
-  QPixmap placeHolder;
-  for (i = 1; i < doc->numPages(); ++i) {
-    offY += pageImage.height() + 10.0;
-
-    newSize = doc->page(i)->pageSizeF();
-    if ( pageSize != newSize ) {
-      std::cerr << "creating new temp page" << std::endl;
-
-      pageSize = newSize;
-      pageSize *= 72.0;
-
-      pageSize.setHeight(pageSize.height() / dpiY);
-      pageSize.setWidth(pageSize.width() / dpiX);
-
-      placeHolder = QPixmap(pageSize.toSize());
-      placeHolder.fill();
-
-    }
-
-    pagePtr = canvas->addPixmap(placeHolder);
+  for (i = 0; i < doc->numPages(); ++i) {
+    pagePtr = new PDFPageGraphicsItem(doc->page(i));
     pagePtr->setPos(0.0, offY);
+
+    canvas->addItem(pagePtr);
+    offY += pagePtr->pixmap().height() + 10.0;
   }
 
-  pagePtr = canvas->items()[0];
-
   viewport->setScene(canvas);
-  viewport->centerOn(pagePtr->boundingRect().center());
 
-  mainWin->setCentralWidget(viewport);
-  mainWin->show();
+  QGraphicsItem *firstPage = viewport->items()[0];
+  viewport->centerOn(firstPage->boundingRect().center());
+
+  mainWin.setCentralWidget(viewport);
+  mainWin.show();
   return app.exec();
   //return 0;
 }
