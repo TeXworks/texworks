7053ef77cc927daccb44f0fbfdd08b31bc1b8fc3
st.loeffler@gmail.com <st.loeffler@gmail.com@9a2a4a0b-1251-0410-9248-99fc04669920>
2013-03-01 20:26:50 +0000

    <QtPDF> Implement zoom in/out
    First cut, works by calling the `scale` method of `QGraphicsView`. Need to figure
    out how to dynamically scale the resolution of rendered PDF pages.

--- modules/QtPDF/poppler_viewer.cpp
+++ modules/QtPDF/poppler_viewer.cpp
@@ -14,13 +14,22 @@ int main(int argc, char **argv) {
   std::cerr << "number of pages: " << doc->numPages() << std::endl;
 
   PDFDocumentView *docView = new PDFDocumentView(doc, &mainWin);
+  mainWin.setCentralWidget(docView);
+
+  QToolBar *toolBar = new QToolBar(&mainWin);
+  toolBar->addAction("Zoom In", docView, SLOT(zoomIn()));
+  toolBar->addAction("Zoom Out", docView, SLOT(zoomOut()));
+  mainWin.addToolBar(toolBar);
 
   PageCounter *counter = new PageCounter(&mainWin);
   counter->setLastPage(docView->lastPage());
-
-  mainWin.setCentralWidget(docView);
   QObject::connect(docView, SIGNAL(changedPage(int)), counter, SLOT(setCurrentPage(int)));
   mainWin.statusBar()->addPermanentWidget(counter);
+
+  ZoomTracker *zoomWdgt = new ZoomTracker(&mainWin);
+  QObject::connect(docView, SIGNAL(changedZoom(qreal)), zoomWdgt, SLOT(setZoom(qreal)));
+  mainWin.statusBar()->addWidget(zoomWdgt);
+
   mainWin.show();
   return app.exec();
   //return 0;
