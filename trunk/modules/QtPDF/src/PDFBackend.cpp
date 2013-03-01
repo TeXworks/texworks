33809cbe8706fd901f1f6d93b34e9062075e776c
st.loeffler@gmail.com <st.loeffler@gmail.com@9a2a4a0b-1251-0410-9248-99fc04669920>
2013-03-01 20:42:59 +0000

    <QtPDF> Update copyrights

--- trunk/modules/QtPDF/src/PDFBackend.cpp
+++ trunk/modules/QtPDF/src/PDFBackend.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2011  Charlie Sharpsteen
+ * Copyright (C) 2011  Charlie Sharpsteen, Stefan Löffler
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
