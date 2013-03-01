fc3fa3c8bdab60c3d7cbd924db40ebf513440f01
st.loeffler@gmail.com <st.loeffler@gmail.com@9a2a4a0b-1251-0410-9248-99fc04669920>
2013-03-01 20:57:32 +0000

    <QtPDF> Split actions & annotations to separate files & thin out #include's

--- trunk/modules/QtPDF/src/PDFBackend.cpp
+++ trunk/modules/QtPDF/src/PDFBackend.cpp
@@ -13,8 +13,8 @@
  */
 
 #include <PDFBackend.h>
-#include <QImage>
 #include <QPainter>
+#include <QApplication>
 
 // TODO: Find a better place to put this
 QBrush * pageDummyBrush = NULL;
@@ -84,151 +84,9 @@ QDateTime fromPDFDate(QString pdfDate)
   return QDateTime(date, time, Qt::UTC).addSecs(sign * (hourOffset * 3600 + minuteOffset * 60)).toLocalTime();
 }
 
-QRectF PDFDestination::viewport(const Document * doc, const QRectF oldViewport, const float oldZoom) const
-{
-  QRectF retVal = oldViewport;
-
-  if (!isValid())
-    return retVal;
-  if (!isExplicit()) {
-    if (!doc)
-      return retVal;
-    return doc->resolveDestination(*this).viewport(doc, oldViewport, oldZoom);
-  }
-
-  // TODO: Fit & FitB (and related) are not strictly the same; Fit refers to
-  // "the entire page", whereas FitB refers to "the bounding box of the content"
-  // TODO: We use values < 0 to reflect pdf (null) values (i.e., invalid values
-  // for top, left, zoom that are to be ignored). Check if negative values are
-  // indeed impossible/not allowed.
-  // FIXME: Below, we const_cast doc because Document::page is non-const. Should
-  // it be?
-  // FIXME: Consistency between retVal < oldViewport and retVal >= oldViewport
-  switch (_type) {
-    case Destination_XYZ:
-    {
-      if (_rect.left() >= 0)
-        retVal.moveLeft(_rect.left());
-      if (_rect.top() >= 0)
-        retVal.moveTop(_rect.top());
-      if (_zoom > 0)
-        retVal = QTransform::fromScale(_zoom / oldZoom, _zoom / oldZoom).mapRect(retVal);
-      break;
-    }
-    case Destination_Fit:
-    case Destination_FitB:
-    {
-      if (!doc)
-        break;
-      Page * p = const_cast<Document*>(doc)->page(_page);
-      if (!p)
-        break;
-      retVal = QRectF(QPointF(0, 0), p->pageSizeF());
-      break;
-    }
-    case Destination_FitH:
-    case Destination_FitBH:
-    {
-      if (!doc)
-        break;
-      Page * p = const_cast<Document*>(doc)->page(_page);
-      if (!p)
-        break;
-      float aspectRatio = oldViewport.width() / oldViewport.height();
-      retVal = QRectF(0, 0, p->pageSizeF().width(), qMin(p->pageSizeF().width() / aspectRatio, p->pageSizeF().height()));
-      break;
-    }
-    case Destination_FitV:
-    case Destination_FitBV:
-    {
-      if (!doc)
-        break;
-      Page * p = const_cast<Document*>(doc)->page(_page);
-      if (!p)
-        break;
-      float aspectRatio = oldViewport.width() / oldViewport.height();
-      retVal = QRectF(0, 0, qMin(p->pageSizeF().height() * aspectRatio, p->pageSizeF().width()), p->pageSizeF().height());
-      break;
-    }
-    case Destination_FitR:
-      retVal = _rect;
-      break;
-  }
-  return retVal;
-}
-
-#ifdef DEBUG
-  QDebug operator<<(QDebug dbg, const PDFDestination & dest)
-  {
-    dbg.nospace() << "PDFDestination(";
-    if (dest.isValid()) {
-      if (dest.isExplicit()) {
-        dbg << dest.page() << " ";
-        switch (dest.type()) {
-          case PDFDestination::Destination_XYZ:
-            dbg << "/XYZ " << dest.left() << " " << dest.top() << " " << dest.zoom();
-            break;
-          case PDFDestination::Destination_Fit:
-            dbg << "/Fit";
-            break;
-          case PDFDestination::Destination_FitH:
-            dbg << "/FitH " << dest.top();
-            break;
-          case PDFDestination::Destination_FitV:
-            dbg << "/FitV " << dest.left();
-            break;
-          case PDFDestination::Destination_FitR:
-            dbg << "/FitR " << dest.left() << " " << dest.rect().bottom() << " " << dest.rect().right() << " " << dest.top();
-            break;
-          case PDFDestination::Destination_FitB:
-            dbg << "/FitB";
-            break;
-          case PDFDestination::Destination_FitBH:
-            dbg << "/FitBH " << dest.top();
-            break;
-          case PDFDestination::Destination_FitBV:
-            dbg << "/FitBV " << dest.left();
-            break;
-        }
-      }
-      else
-        dbg << "name=" << dest.destinationName();
-    }
-    dbg << ")";
-    return dbg.space();
-  }
-#endif // defined(DEBUG)
 
 
 
-// Annotations
-// =================
-
-PDFLinkAnnotation::~PDFLinkAnnotation()
-{
-  if (_actionOnActivation)
-    delete _actionOnActivation;
-}
-
-QPolygonF PDFLinkAnnotation::quadPoints() const
-{
-  if (_quadPoints.isEmpty())
-    return QPolygonF(rect());
-  // The PDF specs (1.7) state that: "QuadPoints should be ignored if any
-  // coordinate in the array lies outside the region specified by Rect."
-  foreach (QPointF p, _quadPoints) {
-    if (!rect().contains(p))
-      return QPolygonF(rect());
-  }
-  return _quadPoints;
-}
-
-void PDFLinkAnnotation::setActionOnActivation(PDFAction * const action)
-{
-  if (_actionOnActivation)
-    delete _actionOnActivation;
-  _actionOnActivation = action;
-}
 
 // Fonts
 // =================
