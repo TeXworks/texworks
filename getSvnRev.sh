41bcf1d9c222c3af8807a9470f879ec1018d58b8
st.loeffler@gmail.com <st.loeffler@gmail.com@9a2a4a0b-1251-0410-9248-99fc04669920>
2013-07-21 09:52:01 +0000

    Improve getSvnRev.sh
    * Avoid touching src/SvnRev.h (which triggers a complete recompile) if unnecessary
    * Clean up displayed message

--- getSvnRev.sh
+++ getSvnRev.sh
@@ -7,30 +7,31 @@ svn update
 
 # get the current revision number
 REV=`LANG=C svn info | fgrep Revision: | cut -d ' ' -f 2`
-
-if [ `svn status | grep -c "^[^?]"` == 0 ]; then
-	# make a new SvnRev.h file
-	echo "#define SVN_REVISION $REV" > src/SvnRev.h.new
-	echo "#define SVN_REVISION_STR \"$REV\"" >> src/SvnRev.h.new
-	# and check if it matches the existing one
-	diff src/SvnRev.h.new src/SvnRev.h > /dev/null
-	if [ $? == 0 ]; then
-		rm src/SvnRev.h.new
-		echo "revision is current"
-	fi
-else
+MODIFIED=0
+if [ `svn status | grep -c "^[^?]"` -ne 0 ]; then
 	# if we have local modifications, we'll bump the revision number
 	REV=$((1+REV))
-	echo "#define SVN_REVISION $REV" > src/SvnRev.h.new
-	echo "#define SVN_REVISION_STR \"$REV\"" >> src/SvnRev.h.new
+	MODIFIED=1
 fi
 
-# is there a new revision file?
-if [ -e src/SvnRev.h.new ]; then
+# make a new SvnRev.h file
+echo "#define SVN_REVISION $REV" > src/SvnRev.h.new
+echo "#define SVN_REVISION_STR \"$REV\"" >> src/SvnRev.h.new
+# and check if it matches the existing one
+diff src/SvnRev.h.new src/SvnRev.h > /dev/null
+if [ $? == 0 ]; then
+	rm src/SvnRev.h.new
+else
 	mv src/SvnRev.h.new src/SvnRev.h
 	VER=`fgrep TEXWORKS_VERSION src/TWVersion.h | cut -d '"' -f 2`
 	sed -e "s/@VER@/$VER/;s/@REV@/$REV/;" <TeXworks.plist.in >TeXworks.plist
 	cp TeXworks.plist Info.plist
-	echo "revision updated"
-	svn status # show status, as a reminder to commit the change(s)
+	echo "updating files...done"
 fi
+
+if [ $MODIFIED -eq 1 ]; then
+	echo "There are local modifications"
+else
+	echo "revision is current"
+fi
+
