7497f8f8eac68a1bbe62c9ba0bb3b6fce0ecb33e
st.loeffler <st.loeffler@9a2a4a0b-1251-0410-9248-99fc04669920>
2014-05-01 20:21:58 +0000

    Fix build fail in r1359

--- src/main.cpp
+++ src/main.cpp
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
 Copyright (C) %1  %2\n\
 License GPLv2+: GNU GPL (version 2 or later) <http://gnu.org/licenses/gpl.html>\n\
 This is free software: you are free to change and redistribute it.\n\
-There is NO WARRANTY, to the extent permitted by law.\n\n").arg("2007-2014", "Jonathan Kew, Stefan Löffler, Charlie Sharpsteen);
+There is NO WARRANTY, to the extent permitted by law.\n\n").arg("2007-2014", "Jonathan Kew, Stefan Löffler, Charlie Sharpsteen");
 			strm.flush();
 		}
 		if ((i = clp.getNextSwitch("help")) >= 0) {
