962807dcd466175904e85e3be720b53e0535ab6d
Dohyun Kim <nomosnomos@gmail.com>
2016-03-13 13:42:04 +0900

    update korean translation

--- modules/QtPDF/trans/QtPDF_ko.ts
+++ modules/QtPDF/trans/QtPDF_ko.ts
@@ -1,257 +1,257 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ko_KR">
+<TS version="2.1" language="ko_KR">
 <context>
     <name>QtPDF</name>
     <message>
         <source>[language name]</source>
-        <translation type="unfinished">한국어</translation>
+        <translation>한국어</translation>
     </message>
     <message>
         <source>[translator&apos;s name/email]</source>
-        <translation type="unfinished">Dohyun Kim&amp;lt;nomos@ktug.org&amp;gt;</translation>
+        <translation>Dohyun Kim&amp;lt;nomos@ktug.org&amp;gt;</translation>
     </message>
 </context>
 <context>
     <name>QtPDF::PDFDocumentScene</name>
     <message>
         <source>rendering page</source>
-        <translation type="unfinished"></translation>
+        <translation>페이지 준비 중</translation>
     </message>
     <message>
         <source>Unlock PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>PDF 잠금 해제</translation>
     </message>
     <message>
         <source>Please enter the password to unlock the PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>PDF 잠금 해제를 위해 패스워드를 입력하세요</translation>
     </message>
     <message>
         <source>Incorrect password</source>
-        <translation type="unfinished"></translation>
+        <translation>패스워드 오류</translation>
     </message>
     <message>
         <source>The password you entered was incorrect.</source>
-        <translation type="unfinished"></translation>
+        <translation>패스워드를 잘못 입력하셨습니다.</translation>
     </message>
     <message>
         <source>This document is locked. You need a password to open it.</source>
-        <translation type="unfinished"></translation>
+        <translation>문서가 잠겨 있습니다. 패스워드가 있어야 문서를 열 수 있습니다.</translation>
     </message>
     <message>
         <source>Unlock</source>
-        <translation type="unfinished"></translation>
+        <translation>잠금 해제</translation>
     </message>
 </context>
 <context>
     <name>QtPDF::PDFDocumentView</name>
     <message>
         <source>Insufficient permission</source>
-        <translation type="unfinished"></translation>
+        <translation>권한 없음</translation>
     </message>
     <message>
         <source>Text extraction is not allowed for this document.</source>
-        <translation type="unfinished"></translation>
+        <translation>이 문서는 텍스트 추출이 허용되지 않습니다.</translation>
     </message>
     <message>
         <source>Goto page %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1쪽으로 가기</translation>
     </message>
     <message>
         <source>Execute `%1`</source>
-        <translation type="unfinished"></translation>
+        <translation>`%1` 실행</translation>
     </message>
     <message>
         <source>Table of Contents</source>
-        <translation type="unfinished"></translation>
+        <translation>차례</translation>
     </message>
     <message>
         <source>Meta Data</source>
-        <translation type="unfinished"></translation>
+        <translation>메타 정보</translation>
     </message>
     <message>
         <source>Processing</source>
-        <translation type="unfinished"></translation>
+        <translation>경과</translation>
     </message>
     <message>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>기타</translation>
     </message>
     <message>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>예</translation>
     </message>
     <message>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>아니오</translation>
     </message>
     <message>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>알 수 없음</translation>
     </message>
     <message>
         <source>Document</source>
-        <translation type="unfinished"></translation>
+        <translation>문서</translation>
     </message>
     <message>
         <source>Title:</source>
-        <translation type="unfinished"></translation>
+        <translation>제목:</translation>
     </message>
     <message>
         <source>Author:</source>
-        <translation type="unfinished"></translation>
+        <translation>작성자:</translation>
     </message>
     <message>
         <source>Subject:</source>
-        <translation type="unfinished"></translation>
+        <translation>주제:</translation>
     </message>
     <message>
         <source>Keywords:</source>
-        <translation type="unfinished"></translation>
+        <translation>키워드:</translation>
     </message>
     <message>
         <source>Creator:</source>
-        <translation type="unfinished"></translation>
+        <translation>작성 도구:</translation>
     </message>
     <message>
         <source>Producer:</source>
-        <translation type="unfinished"></translation>
+        <translation>제작 도구:</translation>
     </message>
     <message>
         <source>Creation date:</source>
-        <translation type="unfinished"></translation>
+        <translation>작성 날짜:</translation>
     </message>
     <message>
         <source>Modification date:</source>
-        <translation type="unfinished"></translation>
+        <translation>수정 날짜:</translation>
     </message>
     <message>
         <source>Trapped:</source>
-        <translation type="unfinished"></translation>
+        <translation>트랩됨:</translation>
     </message>
     <message>
         <source>Fonts</source>
-        <translation type="unfinished">글꼴</translation>
+        <translation>글꼴</translation>
     </message>
     <message>
         <source>Type 0</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Type 1</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Type 1 (multiple master)</source>
-        <translation type="unfinished"></translation>
+        <translation>Type 1 (다중 마스터)</translation>
     </message>
     <message>
         <source>Type 3</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>TrueType</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>yes</source>
-        <translation type="unfinished">예</translation>
+        <translation>예</translation>
     </message>
     <message>
         <source>no</source>
-        <translation type="unfinished">아니오</translation>
+        <translation>아니오</translation>
     </message>
     <message>
         <source>[embedded]</source>
-        <translation type="unfinished">[내장]</translation>
+        <translation>[내장됨]</translation>
     </message>
     <message>
         <source>[builtin]</source>
-        <translation type="unfinished"></translation>
+        <translation>[내장]</translation>
     </message>
     <message>
         <source>Name</source>
-        <translation type="unfinished">이름</translation>
+        <translation>이름</translation>
     </message>
     <message>
         <source>Type</source>
-        <translation type="unfinished">종류</translation>
+        <translation>유형</translation>
     </message>
     <message>
         <source>Subset</source>
-        <translation type="unfinished">서브셋</translation>
+        <translation>서브셋</translation>
     </message>
     <message>
         <source>Source</source>
-        <translation type="unfinished"></translation>
+        <translation>위치</translation>
     </message>
     <message>
         <source>Permissions</source>
-        <translation type="unfinished"></translation>
+        <translation>권한</translation>
     </message>
     <message>
         <source>Allowed</source>
-        <translation type="unfinished"></translation>
+        <translation>허용됨</translation>
     </message>
     <message>
         <source>Low resolution only</source>
-        <translation type="unfinished"></translation>
+        <translation>저해상도만 허용</translation>
     </message>
     <message>
         <source>Denied</source>
-        <translation type="unfinished"></translation>
+        <translation>허용 안됨</translation>
     </message>
     <message>
         <source>Assembling only</source>
-        <translation type="unfinished"></translation>
+        <translation>어셈블리만 허용</translation>
     </message>
     <message>
         <source>Insert, rotate, or delete pages and create bookmarks or thumbnail images</source>
-        <translation type="unfinished"></translation>
+        <translation>페이지 삽입·회전·삭제 및 북마크·썸네일 넣기</translation>
     </message>
     <message>
         <source>Accessibility support only</source>
-        <translation type="unfinished"></translation>
+        <translation>접근성 지원만 허용</translation>
     </message>
     <message>
         <source>Printing:</source>
-        <translation type="unfinished"></translation>
+        <translation>인쇄:</translation>
     </message>
     <message>
         <source>Modifications:</source>
-        <translation type="unfinished"></translation>
+        <translation>내용 변경:</translation>
     </message>
     <message>
         <source>Extraction:</source>
-        <translation type="unfinished"></translation>
+        <translation>텍스트 추출:</translation>
     </message>
     <message>
         <source>Annotation:</source>
-        <translation type="unfinished"></translation>
+        <translation>주석 달기:</translation>
     </message>
     <message>
         <source>Filling forms:</source>
-        <translation type="unfinished"></translation>
+        <translation>양식 채우기:</translation>
     </message>
     <message>
         <source>Annotations</source>
-        <translation type="unfinished"></translation>
+        <translation>주석</translation>
     </message>
     <message>
         <source>Page</source>
-        <translation type="unfinished"></translation>
+        <translation>쪽</translation>
     </message>
     <message>
         <source>Subject</source>
-        <translation type="unfinished"></translation>
+        <translation>이름</translation>
     </message>
     <message>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation>작성자</translation>
     </message>
     <message>
         <source>Contents</source>
-        <translation type="unfinished">차례</translation>
+        <translation>내용</translation>
     </message>
 </context>
 </TS>
