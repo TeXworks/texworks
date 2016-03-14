7b0f03e2a1326fc861d559b4d7985f41a8f6e5f2
mehdioa <mehdioa@gmail.com>
2016-03-14 08:29:39 +0330

    QtPDF_fa.ts revised

--- modules/QtPDF/trans/QtPDF_fa.ts
+++ modules/QtPDF/trans/QtPDF_fa.ts
@@ -1,257 +1,257 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="fa_IR">
+<TS version="2.1" language="fa_IR">
 <context>
     <name>QtPDF</name>
     <message>
         <source>[language name]</source>
-        <translation type="unfinished">پارسی</translation>
+        <translation>پارسی</translation>
     </message>
     <message>
         <source>[translator&apos;s name/email]</source>
-        <translation type="unfinished">مهدی امیدعلی/mehdioa@gmail.com</translation>
+        <translation>مهدی امیدعلی/mehdioa@gmail.com</translation>
     </message>
 </context>
 <context>
     <name>QtPDF::PDFDocumentScene</name>
     <message>
         <source>rendering page</source>
-        <translation type="unfinished"></translation>
+        <translation>ارائهٔ صفحه</translation>
     </message>
     <message>
         <source>Unlock PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>بازکردن پی دی اف</translation>
     </message>
     <message>
         <source>Please enter the password to unlock the PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>برای بازکردن پی دی اف، گذرواژه را وارد نمایید</translation>
     </message>
     <message>
         <source>Incorrect password</source>
-        <translation type="unfinished"></translation>
+        <translation>گذرواژه نادرست است</translation>
     </message>
     <message>
         <source>The password you entered was incorrect.</source>
-        <translation type="unfinished"></translation>
+        <translation>گذرواژهٔ وارد شده نادرست بود.</translation>
     </message>
     <message>
         <source>This document is locked. You need a password to open it.</source>
-        <translation type="unfinished"></translation>
+        <translation>این مستند قفل شده است. برای باز کردن آن به گذرواژه نیاز دارید.</translation>
     </message>
     <message>
         <source>Unlock</source>
-        <translation type="unfinished"></translation>
+        <translation>بازکردن</translation>
     </message>
 </context>
 <context>
     <name>QtPDF::PDFDocumentView</name>
     <message>
         <source>Insufficient permission</source>
-        <translation type="unfinished"></translation>
+        <translation>دسترسی ناکافی است</translation>
     </message>
     <message>
         <source>Text extraction is not allowed for this document.</source>
-        <translation type="unfinished"></translation>
+        <translation>بیرون کشیدن متن از این مستند مجاز نیست.</translation>
     </message>
     <message>
         <source>Goto page %1</source>
-        <translation type="unfinished"></translation>
+        <translation>برو به برگهٔ %1</translation>
     </message>
     <message>
         <source>Execute `%1`</source>
-        <translation type="unfinished"></translation>
+        <translation>اجرای `%1`</translation>
     </message>
     <message>
         <source>Table of Contents</source>
-        <translation type="unfinished"></translation>
+        <translation>فهرست مطالب</translation>
     </message>
     <message>
         <source>Meta Data</source>
-        <translation type="unfinished"></translation>
+        <translation>اَبَرداده</translation>
     </message>
     <message>
         <source>Processing</source>
-        <translation type="unfinished"></translation>
+        <translation>پردازش</translation>
     </message>
     <message>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>دیگری</translation>
     </message>
     <message>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>آری</translation>
     </message>
     <message>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>خیر</translation>
     </message>
     <message>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>ناشناخته</translation>
     </message>
     <message>
         <source>Document</source>
-        <translation type="unfinished"></translation>
+        <translation>مستند</translation>
     </message>
     <message>
         <source>Title:</source>
-        <translation type="unfinished"></translation>
+        <translation>عنوان:</translation>
     </message>
     <message>
         <source>Author:</source>
-        <translation type="unfinished"></translation>
+        <translation>نویسنده:</translation>
     </message>
     <message>
         <source>Subject:</source>
-        <translation type="unfinished"></translation>
+        <translation>موضوع:</translation>
     </message>
     <message>
         <source>Keywords:</source>
-        <translation type="unfinished"></translation>
+        <translation>کلیدواژه‌ها:</translation>
     </message>
     <message>
         <source>Creator:</source>
-        <translation type="unfinished"></translation>
+        <translation>سازنده:</translation>
     </message>
     <message>
         <source>Producer:</source>
-        <translation type="unfinished"></translation>
+        <translation>تولید کننده:</translation>
     </message>
     <message>
         <source>Creation date:</source>
-        <translation type="unfinished"></translation>
+        <translation>تاریخ ساخت:</translation>
     </message>
     <message>
         <source>Modification date:</source>
-        <translation type="unfinished"></translation>
+        <translation>تاریخ اصلاح:</translation>
     </message>
     <message>
         <source>Trapped:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Fonts</source>
-        <translation type="unfinished">قلم‌ها</translation>
+        <translation>قلم‌ها</translation>
     </message>
     <message>
         <source>Type 0</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Type 1</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Type 1 (multiple master)</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Type 3</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>TrueType</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>yes</source>
-        <translation type="unfinished">بله</translation>
+        <translation>آری</translation>
     </message>
     <message>
         <source>no</source>
-        <translation type="unfinished">خیر</translation>
+        <translation>خیر</translation>
     </message>
     <message>
         <source>[embedded]</source>
-        <translation type="unfinished">[نشانده]</translation>
+        <translation>[نشانده]</translation>
     </message>
     <message>
         <source>[builtin]</source>
-        <translation type="unfinished"></translation>
+        <translation>[توکار]</translation>
     </message>
     <message>
         <source>Name</source>
-        <translation type="unfinished">نام</translation>
+        <translation>نام</translation>
     </message>
     <message>
         <source>Type</source>
-        <translation type="unfinished">گونه</translation>
+        <translation>گونه</translation>
     </message>
     <message>
         <source>Subset</source>
-        <translation type="unfinished">زیرگروه</translation>
+        <translation>زیرگروه</translation>
     </message>
     <message>
         <source>Source</source>
-        <translation type="unfinished"></translation>
+        <translation>منبع</translation>
     </message>
     <message>
         <source>Permissions</source>
-        <translation type="unfinished"></translation>
+        <translation>دسترسی‌ها</translation>
     </message>
     <message>
         <source>Allowed</source>
-        <translation type="unfinished"></translation>
+        <translation>مجاز</translation>
     </message>
     <message>
         <source>Low resolution only</source>
-        <translation type="unfinished"></translation>
+        <translation>تنها دقت تصویر کم</translation>
     </message>
     <message>
         <source>Denied</source>
-        <translation type="unfinished"></translation>
+        <translation>پذیرفته نشد</translation>
     </message>
     <message>
         <source>Assembling only</source>
-        <translation type="unfinished"></translation>
+        <translation>تنها سرهم بندی</translation>
     </message>
     <message>
         <source>Insert, rotate, or delete pages and create bookmarks or thumbnail images</source>
-        <translation type="unfinished"></translation>
+        <translation>درج، چرخش، یا پاک کردن برگه‌ها و ساخت چوب‌خط‌ها یا تصویرهای بندانگشتی</translation>
     </message>
     <message>
         <source>Accessibility support only</source>
-        <translation type="unfinished"></translation>
+        <translation>تنها پشتیبانی دست‌یافتنی</translation>
     </message>
     <message>
         <source>Printing:</source>
-        <translation type="unfinished"></translation>
+        <translation>چاپ:</translation>
     </message>
     <message>
         <source>Modifications:</source>
-        <translation type="unfinished"></translation>
+        <translation>اصلاح‌ها:</translation>
     </message>
     <message>
         <source>Extraction:</source>
-        <translation type="unfinished"></translation>
+        <translation>بیرون کشیدن:</translation>
     </message>
     <message>
         <source>Annotation:</source>
-        <translation type="unfinished"></translation>
+        <translation>حاشیه نویسی:</translation>
     </message>
     <message>
         <source>Filling forms:</source>
-        <translation type="unfinished"></translation>
+        <translation>پرکردن فرم‌ها:</translation>
     </message>
     <message>
         <source>Annotations</source>
-        <translation type="unfinished"></translation>
+        <translation>حاشیه نویسی‌ها</translation>
     </message>
     <message>
         <source>Page</source>
-        <translation type="unfinished"></translation>
+        <translation>برگه</translation>
     </message>
     <message>
         <source>Subject</source>
-        <translation type="unfinished"></translation>
+        <translation>موضوع</translation>
     </message>
     <message>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation>نویسنده</translation>
     </message>
     <message>
         <source>Contents</source>
-        <translation type="unfinished">سیاهه</translation>
+        <translation>سیاهه</translation>
     </message>
 </context>
 </TS>
