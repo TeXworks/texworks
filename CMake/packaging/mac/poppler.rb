cf45823f1e6b943b8940e62709593fed510dcc80
vim <st.loeffler@gmail.com>
2017-04-17 18:04:28 +0200

    <travis-ci> Update poppler to 0.53.0 for Windows & OS X

--- CMake/packaging/mac/poppler.rb
+++ CMake/packaging/mac/poppler.rb
@@ -15,16 +15,17 @@
 class Poppler < Formula
   desc "PDF rendering library (based on the xpdf-3.0 code base)"
   homepage "https://poppler.freedesktop.org/"
-  url "https://poppler.freedesktop.org/poppler-0.48.0.tar.xz"
-  sha256 "85a003968074c85d8e13bf320ec47cef647b496b56dcff4c790b34e5482fef93"
+  url "https://poppler.freedesktop.org/poppler-0.53.0.tar.xz"
+  sha256 "592bf72960c6b5948b67657594b05e72d9a278daf7613c9f3cdff9a5b73096a8"
 
 # BEGIN TEXWORKS MODIFICATION
 #  bottle do
-#    sha256 "88dded32895ac3807b5e7496a60f63598c82fceba3b77577d405805b61440a52" => :sierra
-#    sha256 "b705fd24cc2766211c32a0bdc85b07ef084c41130fb4aa3228ecb9da692da7dc" => :el_capitan
-#    sha256 "eb6915ae4a27ddd3c64fc89b7232c31153b8bd6a98de5f379132b956d5cd1150" => :yosemite
+#    rebuild 1
+#    sha256 "5b6de69e6ab1996332934e21b3a5220990e2a5943775d002ee8e5c2447b93758" => :sierra
+#    sha256 "44fcbb473c78c5ce034b65885fcb2e05d4ef0b2d537a043b7d2b6d44d075907b" => :el_capitan
+#    sha256 "47787f7ffa8f69dea50ebb1391186d8146386ad46f2f6b69e3786b7271caeb9d" => :yosemite
 #  end
-  version '0.48.0-texworks'
+  version '0.53.0-texworks'
 
   TEXWORKS_SOURCE_DIR = Pathname.new(__FILE__).realpath.dirname.join('../../..')
   TEXWORKS_PATCH_DIR = TEXWORKS_SOURCE_DIR + 'lib-patches/'
@@ -44,14 +45,13 @@ class Poppler < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "qt4" => :optional
-#  deprecated_option "with-qt4" => "with-qt5"
 # END TEXWORKS MODIFICATION
 
-  option "with-qt5", "Build Qt5 backend"
+  option "with-qt", "Build Qt5 backend"
   option "with-little-cms2", "Use color management system"
 
-  deprecated_option "with-qt" => "with-qt5"
+  deprecated_option "with-qt4" => "with-qt"
+  deprecated_option "with-qt5" => "with-qt"
   deprecated_option "with-lcms2" => "with-little-cms2"
 
   depends_on "pkg-config" => :build
@@ -65,21 +65,21 @@ class Poppler < Formula
   depends_on "libpng"
   depends_on "libtiff"
   depends_on "openjpeg"
-  depends_on "qt5" => :optional
+  depends_on "qt" => :optional
   depends_on "little-cms2" => :optional
 
-  conflicts_with "pdftohtml", :because => "both install `pdftohtml` binaries"
+  conflicts_with "pdftohtml", "pdf2image", "xpdf",
+    :because => "poppler, pdftohtml, pdf2image, and xpdf install conflicting executables"
 
   resource "font-data" do
     url "https://poppler.freedesktop.org/poppler-data-0.4.7.tar.gz"
     sha256 "e752b0d88a7aba54574152143e7bf76436a7ef51977c55d6bd9a48dccde3a7de"
   end
 
+  needs :cxx11 if build.with?("qt") || MacOS.version < :mavericks
+
   def install
-# BEGIN TEXWORKS MODIFICATION
-#    ENV.cxx11 if MacOS.version < :mavericks
-    ENV.cxx11
-# END TEXWORKS MODIFICATION
+    ENV.cxx11 if build.with?("qt") || MacOS.version < :mavericks
     ENV["LIBOPENJPEG_CFLAGS"] = "-I#{Formula["openjpeg"].opt_include}/openjpeg-2.1"
 
     args = %W[
@@ -92,22 +92,15 @@ class Poppler < Formula
       --disable-poppler-qt4
     ]
 
+    if build.with? "qt"
+      args << "--enable-poppler-qt5"
+    else
+      args << "--disable-poppler-qt5"
+    end
+
     args << "--enable-cms=lcms2" if build.with? "little-cms2"
 
 # BEGIN TEXWORKS MODIFICATION
-#    if build.with? "qt5"
-#      args << "--enable-poppler-qt5"
-#    else
-#      args << "--disable-poppler-qt5"
-#    end
-    if build.with? "qt4"
-      args << "--enable-poppler-qt4"
-    elsif build.with? "qt5"
-      args << "--enable-poppler-qt5"
-    else
-      args << "--disable-poppler-qt4" << "--disable-poppler-qt5"
-    end
-
     # We changed the config file (to add native font handling), so we need to
     # update the configure script
     system "autoreconf", "-ivf"
--- travis-ci/mxe/poppler.mk
+++ travis-ci/mxe/poppler.mk
@@ -1,17 +1,18 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := poppler
+$(PKG)_WEBSITE  := https://poppler.freedesktop.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.48.0
-$(PKG)_CHECKSUM := 85a003968074c85d8e13bf320ec47cef647b496b56dcff4c790b34e5482fef93
+$(PKG)_VERSION  := 0.53.0
+$(PKG)_CHECKSUM := 592bf72960c6b5948b67657594b05e72d9a278daf7613c9f3cdff9a5b73096a8
 $(PKG)_SUBDIR   := poppler-$($(PKG)_VERSION)
 $(PKG)_FILE     := poppler-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://poppler.freedesktop.org/$($(PKG)_FILE)
-#$(PKG)_DEPS     := gcc glib cairo libpng lcms1 jpeg tiff freetype zlib curl qt qt5
-$(PKG)_DEPS     := gcc glib cairo libpng lcms jpeg tiff freetype zlib curl qt qt5
+#$(PKG)_DEPS     := gcc cairo curl freetype glib jpeg lcms libpng qt tiff zlib
+$(PKG)_DEPS     := gcc cairo curl freetype glib jpeg lcms libpng qt qtbase tiff zlib
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://poppler.freedesktop.org/' | \
+    $(WGET) -q -O- 'https://poppler.freedesktop.org/' | \
     $(SED) -n 's,.*"poppler-\([0-9.]\+\)\.tar\.xz".*,\1,p' | \
     head -1
 endef
@@ -27,6 +28,8 @@ define $(PKG)_BUILD
         $(MXE_CONFIGURE_OPTS) \
         --disable-silent-rules \
         --enable-xpdf-headers \
+        --enable-poppler-qt4 \
+        --enable-poppler-qt5 \
         --enable-zlib \
         --enable-cms=lcms2 \
         --enable-libcurl \
@@ -46,12 +49,12 @@ define $(PKG)_BUILD
         --disable-gtk-doc-html \
         --disable-gtk-doc-pdf \
         --with-font-configuration=win32 \
-        PKG_CONFIG_PATH_$(subst .,_,$(subst -,_,$(TARGET)))='$(PREFIX)/$(TARGET)/qt5/lib/pkgconfig:$(PREFIX)/$(TARGET)/qt5/lib/pkgconfig' \
-        CXXFLAGS="-D_WIN32_WINNT=0x0500 -std=c++11" \
+        PKG_CONFIG_PATH_$(subst .,_,$(subst -,_,$(TARGET)))='$(PREFIX)/$(TARGET)/qt/lib/pkgconfig:$(PREFIX)/$(TARGET)/qt5/lib/pkgconfig' \
+        CXXFLAGS=-D_WIN32_WINNT=0x0500 \
         LIBTIFF_LIBS="`'$(TARGET)-pkg-config' libtiff-4 --libs`"
     PATH='$(PREFIX)/$(TARGET)/qt/bin:$(PATH)' \
-        $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
-    $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+        $(MAKE) -C '$(1)' -j '$(JOBS)' $(MXE_DISABLE_CRUFT) HTML_DIR=
+    $(MAKE) -C '$(1)' -j 1 install $(MXE_DISABLE_CRUFT) HTML_DIR=
 
     # Test program
     '$(TARGET)-g++' \
