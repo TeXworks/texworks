6236073e2fc9ef8751be479abb332c3647de7d0d
Stefan Löffler <st.loeffler@gmail.com>
2018-03-28 21:53:22 +0200

    <travis-ci> Restrict use of sudo (fixes upload problems to launchpad)
    Sudo is now only used for Windows builds (for which the custom MXE repo
    needs to be added and MXE then needs to be patched).
    Mac OS X does not need sudo at all.
    Linux builds now use the "apt" addon for installing packages.

--- travis-ci/get-dependencies.sh
+++ travis-ci/get-dependencies.sh
@@ -10,18 +10,7 @@ cd "${TRAVIS_BUILD_DIR}"
 print_headline "Getting dependencies for building for ${TARGET_OS}/qt${QT} on ${TRAVIS_OS_NAME}"
 
 if [ "${TARGET_OS}" = "linux" -a "${TRAVIS_OS_NAME}" = "linux" ]; then
-	print_info "Updating apt cache"
-	sudo apt-get -qq update
-	if [ $QT -eq 4 ]; then
-		print_info "Installing packages: ubuntu-dev-tools dput debhelper libqt4-dev zlib1g-dev libhunspell-dev libpoppler-qt4-dev liblua5.2-dev"
-		sudo apt-get install -y ubuntu-dev-tools dput debhelper libqt4-dev zlib1g-dev libhunspell-dev libpoppler-qt4-dev liblua5.2-dev
-	elif [ $QT -eq 5 ]; then
-		print_info "Installing packages: ubuntu-dev-tools dput debhelper qtbase5-dev qtscript5-dev qttools5-dev zlib1g-dev libhunspell-dev libpoppler-qt5-dev libpoppler-private-dev liblua5.2-dev"
-		sudo apt-get install -y ubuntu-dev-tools dput debhelper qtbase5-dev qtscript5-dev qttools5-dev qttools5-dev-tools zlib1g-dev libhunspell-dev libpoppler-qt5-dev libpoppler-private-dev liblua5.2-dev
-	else
-		print_error "Unsupported Qt version '${QT}'"
-		exit 1
-	fi
+	print_info "Nothing to do"
 elif [ "${TARGET_OS}" = "win" -a "${TRAVIS_OS_NAME}" = "linux" ]; then
 	MXEDIR="/usr/lib/mxe"
 	MXETARGET="i686-w64-mingw32.static"
