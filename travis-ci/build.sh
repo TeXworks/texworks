6acfc34ff2c29f79384bf2ffdd340219ad0b1072
Stefan Löffler <st.loeffler@gmail.com>
2018-10-05 08:05:14 +0200

    <travis-ci> Run unit tests on supported platforms
    Windows is not currently supported due to cross-compilation

--- travis-ci/build.sh
+++ travis-ci/build.sh
@@ -12,4 +12,9 @@ print_headline "Building TeXworks for ${TARGET_OS}/qt${QT}"
 echo_and_run "cd \"${BUILDDIR}\""
 echo_and_run "make VERBOSE=1"
 
+# Run unit tests (except for Windows builds which we are cross-compiling)
+if [ ! ("${TARGET_OS}" = "win" -a "${TRAVIS_OS_NAME}" = "linux") ]; then
+	ctest -V
+fi
+
 cd "${TRAVIS_BUILD_DIR}"
