340d81df39571adc80acdba66b7089d20fccc704
Stefan Löffler <st.loeffler@gmail.com>
2018-10-07 17:44:33 +0200

    <travis-ci> Fix some issues in getManual.py detected by a Bandit audit

--- travis-ci/getManual.py
+++ travis-ci/getManual.py
@@ -1,11 +1,25 @@
-import json, sys, os, os.path, zipfile, tempfile
+import json, os, os.path, zipfile, tempfile
 try:
 	# Python3
-	from urllib.request import urlopen, urlretrieve
-except:
+	from urllib.request import build_opener
+except ImportError:
 	# Python2
-	from urllib2 import urlopen
-	from urllib import urlretrieve
+	from urllib2 import build_opener
+
+# Use urllib's build_opener() to define urlopen and urlretrieve as bandit
+# (https://github.com/PyCQA/bandit) does not deem that to be as bad as the
+# built-in variants
+
+url_opener = build_opener()
+
+def urlopen(url):
+	return url_opener.open(url)
+
+def urlretrieve(url, filename):
+	fin = urlopen(url)
+	with open(filename, 'wb') as fout:
+		fout.write(fin.read())
+	fin.close()
 
 
 fin = urlopen("https://api.github.com/repos/TeXworks/manual/releases/latest")
@@ -16,12 +30,12 @@ for asset in release["assets"]:
 	if asset['content_type'] == "application/zip":
 		folder = "texworks-help"
 		if not os.path.exists(folder):
-			os.makedirs()
+			os.makedirs(folder)
 		print("Downloading %s from %s and uncompressing to %s" % (asset['name'], asset['browser_download_url'], folder))
 		tmpfile = tempfile.NamedTemporaryFile(delete = False)
 		tmpfile.close()
 		urlretrieve(asset['browser_download_url'], tmpfile.name)
-		
+
 		with zipfile.ZipFile(tmpfile.name, 'r') as z:
 			z.extractall(folder)
 		os.remove(tmpfile.name)
