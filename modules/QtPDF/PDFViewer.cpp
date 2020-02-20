38aa73faf937d0cad6c01bd34af9b3565932cd93
Stefan Löffler <st.loeffler@gmail.com>
2020-02-20 21:04:35 +0100

    Add missing copyright headers

--- modules/QtPDF/PDFViewer.cpp
+++ modules/QtPDF/PDFViewer.cpp
@@ -1,3 +1,16 @@
+/**
+ * Copyright (C) 2013-2019  Charlie Sharpsteen, Stefan Löffler
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
 #include "PDFViewer.h"
 
 PDFViewer::PDFViewer(const QString & pdf_doc, QWidget *parent, Qt::WindowFlags flags) :
