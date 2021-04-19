487be7a1d38e6fe71a4818e4a4f6fb0135130c5d
Stefan Löffler <st.loeffler@gmail.com>
2021-04-19 18:50:48 +0200

    Fix filtering in the citation select dialog with Qt5
    as reported by Julie Wertz on the mailing list
    [https://tug.org/pipermail/texworks/2021q2/007189.html]
    
    Although QRegularExpression and QRegExp coexist in Qt5, the
    QSortFilterProxyModel only supports querying fixed string filters via
    filterRegExp() until Qt6.

--- src/CitationSelectDialog.cpp
+++ src/CitationSelectDialog.cpp
@@ -313,7 +313,7 @@ bool CitationProxyModel::filterAcceptsRow(int source_row, const QModelIndex &sou
 	static QLatin1String space(" ");
 	const BibTeXFile::Entry * e = static_cast<const BibTeXFile::Entry*>(sourceModel()->index(source_row, 1).internalPointer());
 	QString haystack = e->key() + space + e->typeString() + space + e->author() + space + e->title() + space + e->year() + space + e->howPublished();
-#if QT_VERSION < QT_VERSION_CHECK(5, 12, 0)
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 	QStringList needles = filterRegExp().pattern().split(QChar::fromLatin1(' '), SkipEmptyParts);
 #else
 	QStringList needles = filterRegularExpression().pattern().split(QChar::fromLatin1(' '), SkipEmptyParts);
