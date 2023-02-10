0dffafb49cc34f150db00f4ea43f235be1c09717
Ignasi Furió <ignasi.furio@uib.es>
2023-02-10 21:25:50 +0100

    Update Catalan translation

--- trans/TeXworks_ca.ts
+++ trans/TeXworks_ca.ts
@@ -1132,7 +1132,7 @@ Vol continuar?</translation>
     <message>
         <location filename="../src/PrefsDialog.ui" line="837"/>
         <source>Render Cache:</source>
-        <translation type="unfinished"></translation>
+        <translation>Renderitza Cau:</translation>
     </message>
     <message>
         <location filename="../src/PrefsDialog.ui" line="884"/>
@@ -1187,7 +1187,7 @@ Vol continuar?</translation>
     <message>
         <location filename="../src/PrefsDialog.ui" line="780"/>
         <source>Paper color:</source>
-        <translation type="unfinished"></translation>
+        <translation>Color.de.paper:</translation>
     </message>
     <message>
         <location filename="../src/PrefsDialog.ui" line="790"/>
@@ -1218,7 +1218,7 @@ Vol continuar?</translation>
         <location filename="../src/PrefsDialog.ui" line="850"/>
         <source> MB</source>
         <extracomment>abbreviation of megabytes</extracomment>
-        <translation type="unfinished"></translation>
+        <translation> MB</translation>
     </message>
     <message>
         <location filename="../src/PrefsDialog.ui" line="925"/>
@@ -1843,7 +1843,7 @@ Usi el menú de context per canviar les unitats.</translation>
     <message>
         <location filename="../src/TWApp.cpp" line="445"/>
         <source>&lt;br&gt;Version %1</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;br&gt;Versió %1</translation>
     </message>
     <message>
         <location filename="../src/TWApp.cpp" line="449"/>
