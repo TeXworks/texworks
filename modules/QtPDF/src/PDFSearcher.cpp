57657cb35883c42353e8af4966f4103d36484192
Stefan Löffler <st.loeffler@gmail.com>
2024-01-20 09:20:49 +0100

    Update copyrights

--- modules/QtPDF/src/PDFSearcher.cpp
+++ modules/QtPDF/src/PDFSearcher.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2022  Stefan Löffler
+ * Copyright (C) 2022-2023  Stefan Löffler
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
