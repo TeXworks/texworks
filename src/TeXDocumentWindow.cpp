198614ac6b08b7dee7f02f41d744921ad4b70ee0
Stefan Löffler <st.loeffler@gmail.com>
2024-03-30 14:41:41 +0100

    Remove goto's

--- src/TeXDocumentWindow.cpp
+++ src/TeXDocumentWindow.cpp
@@ -1412,6 +1412,11 @@ void TeXDocumentWindow::pdfClosed()
 
 bool TeXDocumentWindow::saveFile(const QFileInfo & fileInfo)
 {
+	const auto showNotSavedMessage = [this]() {
+		statusBar()->showMessage(tr("Document \"%1\" was not saved")
+									 .arg(textDoc()->getFileInfo().fileName()),
+								 kStatusMessageDuration);
+	};
 	QDateTime fileModified = fileInfo.lastModified();
 	if (fileInfo == textDoc()->getFileInfo() && fileModified.isValid() && fileModified != lastModified) {
 		if (QMessageBox::warning(this, tr("File changed on disk"),
@@ -1419,11 +1424,8 @@ bool TeXDocumentWindow::saveFile(const QFileInfo & fileInfo)
 									"Do you want to proceed with saving this file, overwriting the version on disk?")
 								 .arg(fileInfo.absoluteFilePath()),
 								 QMessageBox::Ok | QMessageBox::Cancel, QMessageBox::Cancel) == QMessageBox::Cancel) {
-			notSaved:
-				statusBar()->showMessage(tr("Document \"%1\" was not saved")
-										 .arg(textDoc()->getFileInfo().fileName()),
-										 kStatusMessageDuration);
-				return false;
+			showNotSavedMessage();
+			return false;
 		}
 	}
 
@@ -1447,8 +1449,10 @@ bool TeXDocumentWindow::saveFile(const QFileInfo & fileInfo)
 				   "If you proceed, they will be replaced with default codes. "
 				   "Alternatively, you may wish to use a different encoding (such as UTF-8) to avoid loss of data.")
 		            .arg(QString::fromUtf8(codec->name().constData())),
-				QMessageBox::Ok | QMessageBox::Cancel, QMessageBox::Cancel) == QMessageBox::Cancel)
-			goto notSaved;
+				QMessageBox::Ok | QMessageBox::Cancel, QMessageBox::Cancel) == QMessageBox::Cancel) {
+			showNotSavedMessage();
+			return false;
+		}
 	}
 
 	clearFileWatcher();
@@ -1461,7 +1465,8 @@ bool TeXDocumentWindow::saveFile(const QFileInfo & fileInfo)
 								 tr("Cannot write file \"%1\":\n%2")
 								 .arg(fileInfo.absoluteFilePath(), file.errorString()));
 			setupFileWatcher();
-			goto notSaved;
+			showNotSavedMessage();
+			return false;
 		}
 
 		QApplication::setOverrideCursor(Qt::WaitCursor);
@@ -1479,7 +1484,8 @@ bool TeXDocumentWindow::saveFile(const QFileInfo & fileInfo)
 								 tr("An error may have occurred while saving the file. "
 									"You might like to save a copy in a different location."),
 								 QMessageBox::Ok);
-			goto notSaved;
+			showNotSavedMessage();
+			return false;
 		}
 		file.commit();
 		QApplication::restoreOverrideCursor();
@@ -2340,12 +2346,19 @@ void TeXDocumentWindow::doFindAgain(bool fromDialog)
 	}
 
 	if (fromDialog && (settings.value(QString::fromLatin1("searchFindAll")).toBool() || settings.value(QString::fromLatin1("searchAllFiles")).toBool())) {
-		bool singleFile = true;
 		QList<SearchResult> results;
 		flags &= ~QTextDocument::FindBackward;
-		int docListIndex = 0;
-		TeXDocumentWindow* theDoc = this;
-		while (true) {
+
+		QList<TeXDocumentWindow*> docsToSearch{this};
+		if (settings.value(QString::fromLatin1("searchAllFiles")).toBool()) {
+			for (TeXDocumentWindow * theDoc : docList) {
+				if (!docsToSearch.contains(theDoc)) {
+					docsToSearch.append(theDoc);
+				}
+			}
+		}
+
+		for (TeXDocumentWindow * theDoc : docsToSearch) {
 			QTextCursor curs(theDoc->textDoc());
 			curs.movePosition(QTextCursor::End);
 			int rangeStart = 0;
@@ -2362,19 +2375,6 @@ void TeXDocumentWindow::doFindAgain(bool fromDialog)
 				else
 					rangeStart = curs.selectionEnd();
 			}
-
-			if (!settings.value(QString::fromLatin1("searchAllFiles")).toBool())
-				break;
-			// go to next document
-		next_doc:
-			if (docList[docListIndex] == theDoc)
-				docListIndex++;
-			if (docListIndex == docList.count())
-				break;
-			theDoc = docList[docListIndex];
-			if (theDoc == this)
-				goto next_doc;
-			singleFile = false;
 		}
 
 		if (results.count() == 0) {
@@ -2382,6 +2382,7 @@ void TeXDocumentWindow::doFindAgain(bool fromDialog)
 			statusBar()->showMessage(tr("Not found"), kStatusMessageDuration);
 		}
 		else {
+			const bool singleFile = (docsToSearch.size() == 1);
 			SearchResults::presentResults(searchText, results, this, singleFile);
 			statusBar()->showMessage(tr("Found %n occurrence(s)", "", static_cast<int>(results.count())), kStatusMessageDuration);
 		}
