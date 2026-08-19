e6a1052db6f1a04bf2a9aa78e5601bc43f06b68c
Stefan Löffler <st.loeffler@gmail.com>
2026-08-19 15:55:19 +0200

    Always use cleaned paths for comparisons of FileVersionDatabase::Record
    QFileInfo::absoluteFilePath() is not guaranteed to resolve redundant "." and
    ".." directories. However, we a) may need to compare simple strings (in
    case of non-existent files) and b) want to avoid crossing into
    directories we don't have permission to access or which are on different
    file systems (particularly if we need to traverse up to the root
    directory and/or run unit-tests on automated build systems)

--- unit-tests/Utils_test.cpp
+++ unit-tests/Utils_test.cpp
@@ -43,24 +43,36 @@ namespace Tw {
 namespace Utils {
 bool operator==(const FileVersionDatabase::Record & r1, const FileVersionDatabase::Record & r2)
 {
-	if (r1.version != r2.version || r1.hash != r2.hash)
+	auto cleanAbsoluteFileInfo = [](const FileVersionDatabase::Record & rec) {
+		const QDir dir{QDir::cleanPath(rec.filePath.absolutePath())};
+		return QFileInfo(dir, rec.filePath.fileName());
+	};
+	if (r1.version != r2.version || r1.hash != r2.hash) {
 		return false;
+	}
 
 	// Work around the fact that the behavior of QFileInfo::operator== is
 	// undefined if the objects are empty or refer to non-existant files
+	// Also, comparisons might break if paths are not cleaned and the traversal
+	// of the uncleaned path crosses into inaccessible territory (e.g., where
+	// the user has insufficient permissions or when crossing boundaries of
+	// mounted file systems)
+
+	const QFileInfo fi1 = cleanAbsoluteFileInfo(r1);
+	const QFileInfo fi2 = cleanAbsoluteFileInfo(r2);
 
 	// If the only one of the files exists, they are clearly not the same
-	if (r1.filePath.exists() != r2.filePath.exists())
+	if (fi1.exists() != fi2.exists()) {
 		return false;
+	}
 
-	if (r1.filePath.exists()) {
+	if (fi1.exists()) {
 		// If they exist, we can compare them directly
-		return (r1.filePath == r2.filePath);
+		return (fi1 == fi2);
 	}
 	else {
-		// If they don't exist, we compare the stored absolute file paths
-		// (which can be empty)
-		return (r1.filePath.absolutePath() == r2.filePath.absolutePath());
+		// If they don't exist, we compare the path strings (which can be empty).
+		return (fi1.filePath() == fi2.filePath());
 	}
 }
 bool operator==(const FileVersionDatabase & db1, const FileVersionDatabase & db2) {
